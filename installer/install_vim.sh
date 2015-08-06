2948baae0f77124609f7d7640dfcc2a7457cc8c5
Jui-Hsien Wang <jw969@sh-5-35.local>
2015-08-05 20:19:06 -0700

    refactor. change bashrc so its more general

---
+++ installer/install_vim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PWD=`pwd`
+DST_DIR=${HOME}/opt
+
+if [ ! -d ../package ]; then
+    mkdir ../package
+fi
+
+echo "Entering package folder and download/install vim at $DST_DIR"
+cd ../package
+
+hg clone https://vim.googlecode.com/hg/ vim
+
+cd vim
+
+./configure --prefix=${DST_DIR}
+make
+make install
+
+
+cd ${PWD}
